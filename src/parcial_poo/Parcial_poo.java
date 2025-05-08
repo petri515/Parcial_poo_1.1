f199f129889cb3334f8e461d063d7fc61e6b8409
estudiante <estudiante@10.220.4.133>
2025-05-08 17:57:51 -0500

    2. segundo ejecutable

--- src/parcial_poo/Parcial_poo.java
+++ src/parcial_poo/Parcial_poo.java
@@ -15,20 +15,58 @@ public class Parcial_poo {
   
     
     public static void main(String[] args) {
-        while(true){
-        System.out.println("Ingrese el numero segun la operacion que desee realizar \n 1. Registrar nuevo libro \n 2. Registrar nuevo usuario \n 3. buscar categoria \n 0.Salir del programa ");
-        Scanner sc = new Scanner(System.in);
-        
+        OUTER:
+        while (true) {
+            System.out.println("Ingrese el numero segun la operacion que desee realizar \n 1. Registrar nuevo libro \n 2. Registrar nuevo usuario \n 3. buscar por categoria \n 0.Salir del programa ");
+            Scanner sc = new Scanner(System.in);
             int ejecutar = sc.nextInt();
-        
-            if(ejecutar == 1){
-                libros libro1 = new libros();
-                System.out.print("El libro se llamara: ");
-                String nombre = sc.next();
-                libro1.setNombreLibro(nombre);
             
-        
-        }
+            switch (ejecutar) {
+                case 1 -> {
+                    libros libro1 = new libros();
+                    
+                    System.out.print("El libro se llamara: ");
+                    
+                    String nombre = sc.next();
+                    
+                    libro1.setNombreLibro(nombre);
+                    
+                    System.out.print("El libro es de tipo: \n 1. Drama \n 2.Comedia \n 3. Aventura \n ");
+                    
+                    int tipo_ = sc.nextInt();
+                    
+                    String tipo = null;
+                    
+                    switch(tipo_){
+                        case 1 ->{                            
+                            tipo = "Drama";                                   
+                        }
+                        case 2 ->{
+                            tipo = "Comedia";
+                        }
+                        case 3 ->{
+                            tipo = "Aventura";
+                        }
+                    }
+                    libro1.setTipoLibro(tipo);
+                }
+                case 2 -> {
+                    Usuario user_1 = new Usuario();
+                    System.out.print("El nuevo usuario se llama: ");
+                    String name_user = sc.next();
+                    user_1.setname(name_user);
+                    System.out.print("Con la identificacion: ");
+                    String id_user = sc.next();
+                    user_1.setId_user(id_user);
+                }
+
+                case 0 -> {
+                    System.out.println("Saliendo del programa...");
+                    break OUTER;
+                }
+                default -> {
+                }
+            }
         }
     }   
      
